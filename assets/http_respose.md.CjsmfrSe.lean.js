1c7dbff2068741ace591838107bb3e6da50d19e5
86kkd <97971411+86kkd@users.noreply.github.com>
2024-12-18 05:52:34 +0000

    Deploying to gh-pages from @ 86kkd/86kkd.blog@208516f2578cf5fc6f19605b9262154486b84e00 🚀

---
+++ assets/http_respose.md.CjsmfrSe.lean.js
@@ -0,0 +1,15 @@
+import{_ as n,c as a,j as e,a as r,o}from"./chunks/framework.C3rjE0BJ.js";const m=JSON.parse('{"title":"HTTP状态码","description":"","frontmatter":{},"headers":[],"relativePath":"http/respose.md","filePath":"http/respose.md","lastUpdated":1734501116000}'),s={name:"http/respose.md"};function d(p,t,l,i,c,h){return o(),a("div",null,t[0]||(t[0]=[e("h1",{id:"http状态码",tabindex:"-1"},[r("HTTP状态码 "),e("a",{class:"header-anchor",href:"#http状态码","aria-label":'Permalink to "HTTP状态码"'},"​")],-1),e("pre",null,[e("code",null,`2xx：成功
+    200: OK
+    201: Created
+    204: No Content
+
+4xx：客户端错误
+    400: Bad Request
+    401: Unauthorized（未认证）
+    403: Forbidden（无权限）
+    404: Not Found
+
+5xx：服务器错误
+    500: Internal Server Error
+    503: Service Unavailable
+`)],-1)]))}const u=n(s,[["render",d]]);export{m as __pageData,u as default};
